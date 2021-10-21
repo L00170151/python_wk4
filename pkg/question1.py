13669e3705e92f3c372b16fcd6095b6144b4fcd7
Eoin <L00170151@student.lyit.ie>
2021-10-21 14:15:03 +0100

    initial code

---
+++ pkg/question1.py
@@ -0,0 +1,19 @@
+"""
+#
+File    :question1.py
+Created :14/10/2021
+Author  : Eoin O'Mahony
+"""
+import ntpath
+import os
+import sys
+# import modulefinder
+# import pathlib
+
+if __name__ == '__main__':
+    my_os = os.uname()
+    my_path =  sys.path
+    path_to_pkg = my_path[0]
+
+    print(f"My path to packge is {path_to_pkg}, my OS is {my_os[0]}, my node is {my_os[1]} ")
+
---
+++ pkg/question2.py
@@ -0,0 +1,30 @@
+"""
+#
+File    :question2.py
+Created :21/10/2021
+Author  : Eoin O'Mahony
+"""
+
+import crypt
+import timeit
+
+
+def test():
+    # password = str(input("Please type in your password:"))
+    password = "abcdefghijglmnop"
+    encrypted_password = crypt.crypt(password)
+    print("Pre encryption: " + password)
+    print("Post encryption: " + encrypted_password)
+
+def timing():
+    print("Time taken to encrypt: "+ str(timeit.timeit(stmt=test, number=1)))
+    return
+
+
+timing()
+
+
+
+
+
+
